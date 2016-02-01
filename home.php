db56a91b59ca76381eebfe90cfa789cc4e53d462
Jim Barnes <james.barnes@ucf.edu>
2016-02-01 14:33:09 -0500

    Updated template for home page.

--- home.php
+++ home.php
@@ -1,6 +1,17 @@
 <?php disallow_direct_load( 'home.php' ); ?>
 <?php get_header(); the_post(); ?>
+<primary>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<h2><?php echo the_title(); ?></h2>
+				<?php echo the_content(); ?>
+			</div>
+			<div class="col-md-4">
 
-<?php echo the_content(); ?>
+			</div>
+		</div>
+	</div>
+</primary>
 
 <?php get_footer(); ?>
