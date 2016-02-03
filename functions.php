124dbb9edffe966bba1319841ff2f83e62533688
Keegan Berry <keeganberry@gmail.com>
2016-02-03 17:04:56 -0500

    swapped fa fonts out for weather icons and organized icon output function in the header

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ require_once 'functions/config.php';        // Where per theme settings are regi
 require_once 'functions/theme.php';         // Theme-specific functions should be added here
 require_once 'functions/admin.php';         // Admin/login functions
 require_once 'shortcodes.php';              // Per theme shortcodes
- 
+
 /**
  * NOTE: functions specific to this theme should be defined in
  * functions/theme.php instead of this file.  Note the load order of files
@@ -36,31 +36,98 @@ function get_weather_data() {
 	);
 
 	$context = stream_context_create( $opts );
-
 	$file = file_get_contents( get_theme_mod_or_default( 'weather_feed_url' ), false, $context );
-
 	$weather = json_decode( $file );
 
-	switch ($weather->condition) {
-		case 'Sunny':
-			$weather->icon = 'sun-o';
-			break;
-		case 'Foggy':
-		case 'Cloudy':
-			$weather->icon = 'cloud';
-			break;
-		case 'Stormy':
-			$weather->icon = 'bolt';
-			break;
-		case 'default':
-			$weather->icon = 'sun-o';
-			break;
-		default:
-			$weather->icon = 'sun-o';
-			break;
-	}
+	$weather->icon = get_weather_icon( $weather->condition );
 
 	return $weather;
+}
+
+function get_weather_icon( $condition ) {
+	// https://erikflowers.github.io/weather-icons/
+	// TODO: add more icons to this now that we're using weather icons
+
+	$icon_prefix = "wi wi-";
+	$icons_to_conditions = array(
+			'day-sunny' => array(
+				'sunny',
+				'fair (day)',
+				'fair',
+				'default'
+			),
+			'night-clear' => array(
+				'clear (night)',
+				'fair (night)',
+				'hot',
+				'night'
+			),
+			'cloudy' => array(
+				'dust',
+				'foggy',
+				'haze',
+				'smoky',
+				'blustery',
+				'windy',
+				'foggy',
+				'cold',
+				'cloudy',
+				'mostly cloudy (night)',
+				'mostly cloudy (day)',
+				'partly cloudy (night)',
+				'partly cloudy (day)',
+				'partly cloudy',
+				'partly cloudy'
+			),
+			'showers' => array(
+				'mixed rain and snow',
+				'mixed rain and sleet',
+				'mixed snow and sleet',
+				'freezing drizzle',
+				'drizzle',
+				'freezing rain',
+				'showers',
+				'showers',
+				'snow flurries',
+				'light snow showers',
+				'blowing snow',
+				'snow',
+				'hail',
+				'sleet',
+				'mixed rain and hail',
+				'rainy',
+				'thundershowers',
+				'snow showers',
+				'isolated thundershowers'
+			),
+			'lightning' => array(
+				'isolated thunderstorms',
+				'scattered thunderstorms',
+				'scattered thunderstorms',
+				'scattered showers',
+				'heavy snow',
+				'scattered snow showers',
+				'heavy snow',
+				'tornado',
+				'tropical storm',
+				'hurricane',
+				'severe thunderstorms',
+				'thunderstorms',
+				'stormy'
+			)
+		);
+
+	$condition = strtolower( $condition );
+
+	foreach ( $icons_to_conditions as $icon => $condition_array ) {
+		if ( in_array( $condition, $condition_array ) ) {
+			return $icon_prefix . $icon;
+		}
+	}
+
+	// If the condition for some reason isn't listed here,
+	// no icon name will be returned and so no icon will be used
+	return false;
 }
 
 ?>
