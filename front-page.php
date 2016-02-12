a19ce007076ba75b8f3dd025001ddcfa9253a521
Jim Barnes <james.barnes@ucf.edu>
2016-02-12 15:18:05 -0500

    Stylistic updates.

--- front-page.php
+++ front-page.php
@@ -2,19 +2,17 @@
 <?php get_header(); the_post(); ?>
 <article>
 	<div class="container">
-		<div class="row">
-			<div class="col-md-7 col-sm-6 col-xs-12">
-				<div class="page-wrap">
-					<h2><?php echo the_title(); ?></h2>
-					<div class="content">
-						<?php
-							$message = get_field( 'page_message' );
-							echo apply_filters( 'the_content', $message );
-						?>
-					</div>
+		<div class="row page-wrap">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<h2><?php echo the_title(); ?></h2>
+				<div class="content">
+					<?php
+						$message = get_field( 'page_message' );
+						echo apply_filters( 'the_content', $message );
+					?>
 				</div>
 			</div>
-			<div class="col-md-5 col-sm-6 col-xs-12">
+			<div class="col-md-4 col-md-offset-1 col-sm-6 col-xs-12">
 				<?php
 					$spotlight = get_field( 'page_spotlight' );
 					if ( $spotlight ) {
