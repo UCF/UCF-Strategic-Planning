af548005974bc6a973527e2728fa4d4440be3909
Keegan Berry <keeganberry@gmail.com>
2016-02-25 09:39:39 -0500

    a few fixes

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,7 @@ var headerImage = function($) {
 	var resize = function() {
 		var $image = $('.header-image'),
 			width = $image.width(),
-			height = (width * 0.375).clamp(400, 750); //.clamp(600, 1125);  //clamp(400, 750);
+			height = (width * 0.375).clamp(400, 750);
 
 		$image.height(height + 'px');
 	};
