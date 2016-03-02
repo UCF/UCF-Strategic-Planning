ccf0fff3869f122550b0c43b5af84a6c25b81789
Keegan Berry <keeganberry@gmail.com>
2016-03-02 10:44:48 -0500

    type fix

--- front-page.php
+++ front-page.php
@@ -26,7 +26,7 @@
 	<nav id="sections-navbar" class="navbar navbar-gold center">
 		<div class="container-fluid">
 			<div class="navbar-header">
-				<span class="navbar-title">Skip-To Section</span>
+				<span class="navbar-title">Skip To Section</span>
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sections-menu">
 					<span class="sr-only">Toggle navigation</span>
 					<span class="icon-bar"></span>
