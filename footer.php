3af1f22d3260b17432498e74ffd6a0d4038d46a5
Jo Dickson <jogreybill@gmail.com>
2017-03-22 15:24:17 -0500

    Removed custom footer markup/styles in favor of using the UCF-Footer-Plugin

--- footer.php
+++ footer.php
@@ -1,13 +1,4 @@
 		</main>
-		<footer class="site-footer">
-			<div class="main-site-footer">
-				<div class="container">
-					<p class="main-site-title">University of Central Florida</p>
-					<?php display_social(); ?>
-					<?php display_footer_menu() ; ?>
-				</div>
-			</div>
-			<?php wp_footer(); ?>
-		</footer>
+		<?php wp_footer(); ?>
 	</body>
 </html>
